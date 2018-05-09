0a8b451f1c4bb72e0457642ed6b909d88cd08fa9
David <dmchoull@gmail.com>
2018-05-09 15:04:50 -0400

    [dm gc] Moved en strings into separate file

---
+++ src/translations/en.js
@@ -0,0 +1,64 @@
+/* eslint-disable max-len */
+export default {
+  translations: {
+    nav: {
+      links: {
+        whitepaper: 'Whitepaper',
+        overview: 'Overview',
+      },
+    },
+
+    home: {
+      headline: 'A decentralized currency based on blockchain technology powering NextGen Semantic Web',
+      viewWhitepaper: 'View Whitepaper',
+      subscribeButton: 'Register for ICO',
+
+      valueProp: {
+        shareEconomy: {
+          title: 'A Share Economy for Untapped Computing Power and Human Intelligence',
+          description: 'End users can earn Tethys tokens for sharing bandwidth and resources, enabling rapid growth at a low cost.',
+        },
+        semanticWeb: {
+          title: 'An Artificial Intelligence to Navigate and Organize Semantic Information',
+          description: 'Robust extraction of complex semantic information powered by an unparalleled modular deep learning model designed to simplify the implementation and execution of A.I.',
+        },
+        infinitelyScalable: {
+          title: 'An Infinitely Scalable Blockchain to Bring Long-term Memory to the Web',
+          description: 'Serves as a memory base for the web in which users can monitor changes at any point in time. Supported by unique blockchain technology to store vast amounts of information on the web',
+        },
+      },
+
+      learnMore: 'Learn More About Tethys',
+
+      subscribeForm: {
+        title: 'Subscribe for Updates',
+        email: { placeholder: 'Enter your email' },
+        selectCountry: '– Country of residence –',
+        wontShareEmail: 'We won\'t share your email address or send you any spam',
+      },
+
+      links: {
+        title: 'Links',
+        github: 'Github',
+        telegram: 'Telegram',
+        medium: 'Medium',
+      },
+    },
+
+    countDown: {
+      heading: 'Pre-sale starts {{endDate}}',
+      days: 'Days',
+      hours: 'Hours',
+      minutes: 'Minutes',
+      seconds: 'Seconds',
+    },
+
+    footer: {
+      privacyPolicy: 'Privacy Policy',
+      terms: 'Terms',
+      copyright: '© 2018 Tethys',
+    },
+
+    submit: 'Submit',
+  },
+};
