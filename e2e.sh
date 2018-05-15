53db4f5d5363071a6c40710e74432e872573d94c
David <dmchoull@gmail.com>
2018-05-14 22:13:19 -0400

    [dm] make e2e script wait for server to start up before running cypress

--- e2e.sh
+++ e2e.sh
@@ -3,6 +3,12 @@ set -ev
 
 yarn build --staging
 yarn serve &
+
+# wait for serve to start
+while ! curl -s localhost:3000 >/dev/null; do
+  sleep 1
+done
+
 yarn run cypress run
 
 # kill serve process
