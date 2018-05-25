9a08e1d00e8df0b0b72c4329c6f73e3b53866f1a
David <dmchoull@gmail.com>
2018-05-25 17:25:11 -0400

    [dm] configure postcss-custom-properties plugin through postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,23 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    // temporarily disable custom-properties here because it is added again below
-    // change this once there's a way to pass options to plugins in preset-env options
-    'postcss-preset-env': { stage: 1, features: { 'custom-properties': false } },
-    'postcss-custom-properties': {
-      preserve: false,
-      variables: {
-        backgroundColor: '#29292F',
-        backgroundDarkColor: 'rgba(0, 0, 0, 0.16)',
-        mainTextColor: '#B0B0B0',
-        headingTextColor: '#FFF',
-        inputBackgroundColor: 'rgba(255, 255, 255, 0.2)',
-        inputTextColor: 'rgba(255, 255, 255, 1)',
-        inputPlaceholderColor: 'rgba(255, 255, 255, 0.2)',
-        tethysBlue: '#30f1fd',
-        tethysBlueBorder: 'rgba(48, 241, 253, 0.3)',
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'custom-properties': {
+          preserve: false,
+          variables: {
+            backgroundColor: '#29292F',
+            backgroundDarkColor: 'rgba(0, 0, 0, 0.16)',
+            mainTextColor: '#B0B0B0',
+            headingTextColor: '#FFF',
+            inputBackgroundColor: 'rgba(255, 255, 255, 0.2)',
+            inputTextColor: 'rgba(255, 255, 255, 1)',
+            inputPlaceholderColor: 'rgba(255, 255, 255, 0.2)',
+            tethysBlue: '#30f1fd',
+            tethysBlueBorder: 'rgba(48, 241, 253, 0.3)',
+          },
+        },
       },
     },
     'autoprefixer': { flexbox: 'no-2009' },
