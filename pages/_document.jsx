06edcfe7d5e70abd594784a454e2377535b196ab
David <dmchoull@gmail.com>
2018-06-22 09:34:49 -0400

    feat: add sentry error reporting

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 
-const polyfillURL = 'https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.find,' +
+const polyfillURL =
+  'https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.find,' +
   'Array.prototype.findIndex,Array.prototype.includes';
 
 const GA_ID = 'UA-50367475-9';
@@ -21,21 +22,20 @@ export default class MyDocument extends Document {
 
           <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-          {
-            process.env.ENABLE_ANALYTICS === 'true' &&
+          {process.env.ENABLE_ANALYTICS === 'true' && (
             <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
-          }
+          )}
 
-          {
-            process.env.ENABLE_ANALYTICS === 'true' &&
-            <script dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
+          {process.env.ENABLE_ANALYTICS === 'true' && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments)}
                       gtag('js', new Date());
                       gtag('config', '${GA_ID}');`.replace(/\s{2,}/g, ''),
-            }}
+              }}
             />
-          }
+          )}
 
           <script src={polyfillURL} />
 
@@ -53,6 +53,20 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+
+          {process.env.ENABLE_SENTRY === 'true' && (
+            <React.Fragment>
+              <script src="https://cdn.ravenjs.com/3.26.2/raven.min.js" crossOrigin="anonymous" />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `Raven.config('https://29a59bb1f2f54d5ea36aef5fa9a915fa@sentry.io/1230509', {
+                  environment: '${process.env.NODE_ENV}',
+                }).install()`.replace(/\s{2,}/g, ''),
+                }}
+              />
+            </React.Fragment>
+          )}
+
           <NextScript />
         </body>
       </html>
